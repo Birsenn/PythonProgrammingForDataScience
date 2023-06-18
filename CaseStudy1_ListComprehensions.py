3bc26268b962a1209fb0cf901bb66f316c68083e
Birsen Bayat <brsnbas@gmail.com>
2023-06-18 17:44:10 +0300

    Create CaseStudy1_ListComprehensions.py

---
+++ CaseStudy1_ListComprehensions.py
@@ -0,0 +1,22 @@
+#1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini büyük harfe çeviriniz
+#ve başına NUM ekleyiniz.
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+df.columns = ["NUM_" + col.upper() if df[col].dtype != "O" else col.upper() for col in df.columns]
+
+##########################################################################################################################################
+#2: List Comprehension yapısı kullanarak car_crashes verisinde isminde "no" barındırmayan değişkenlerin isimlerinin sonuna "FLAG" yazınız.
+df = sns.load_dataset("car_crashes")
+
+df.columns = [col.upper() + "_FLAG" if "no" not in col else col.upper() for col in df.columns]
+
+############################################################################################################################################
+#3: List Comprehension yapısı kullanarak aşağıda verilen değişken isimlerinden FARKLI olan değişkenlerin isimlerini seçiniz
+#ve yeni bir dataframe oluşturunuz.
+
+og_list = ["abbrev", "no_previous"]
+new_cols = [col for col in df.columns if col not in og_list]
+new_df = df[new_cols]
